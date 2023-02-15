bda4258b45b00a2a58abced06a43a5e3c019afed
HemillyJulia <hemillyjulia@hotmail.com>
2023-02-15 17:10:26 -0500

    Componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components"
+import { Transactions } from "./pages/Transactions"
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 
@@ -8,7 +9,7 @@ import { defaultTheme } from "./styles/themes/default"
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle/>
-   <h1>Oi lindo mundo!</h1>
+   <Transactions/>
    </ThemeProvider>
   )
 }
