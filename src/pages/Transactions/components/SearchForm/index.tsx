a447c65185b4fc722e3e30344d91691f32891eb9
HemillyJulia <hemillyjulia@hotmail.com>
2023-03-21 22:58:40 -0400

    React Hook Form

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -18,7 +18,7 @@ export function SearchForm (){
     })
 
        async function handleSearchTransactions(data:SearchFormInputs){
-        await new Promise (resolve => setTimeout(resolve,2000))
+        
             console.log(data)
     }
 
